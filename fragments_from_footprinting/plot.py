2005960f920bebe9a64dab141ed199f933ed1980
arianabrenner <arianaebrenner@gmail.com>
2023-03-07 12:40:12 -0500

    added plot_composite func

--- fragments_from_footprinting/plot.py
+++ fragments_from_footprinting/plot.py
@@ -46,6 +46,31 @@ mpl.rcParams['font.sans-serif'] = 'Helvetica'
 # Set color-blindness friendly color palette
 mpl.rcParams['axes.prop_cycle'] = cycler(color=['#0072B2', '#D55E00', '#009E73', '#CC79A7','darkgrey', '#56B4E9','#E69F00','#F0E442']) # can add black: '#000000'
 
+def process_vplot_data(vplot_data: np.ndarray):
+    """
+    Normalize vplot data and rotate the matrix for later plotting with plt.imshow()
+    
+    Parameters
+    ----------
+    vplot_data : np.ndarray
+    2D Numpy array that contains the data to be plotted.
+
+    Returns
+    -------
+    array_to_plot : np.ndarray
+    Normalized and rotated vplot_data
+    """
+
+    # Process v-plot count data for plotting
+    # imshow orients plots differently than the 2D histogram code used to generate array
+    # Here we reorient the data for ploting with imshow
+    vplot_data_rotated = np.flip(vplot_data.T)
+    # Min-max normalize counts
+    min_max_scaler = preprocessing.MinMaxScaler()
+    array_to_plot = min_max_scaler.fit_transform(vplot_data_rotated)
+    return array_to_plot
+
+
 def plot_vplot(vplot_data: np.ndarray):
     """
     Generate a v_plot pdf that contains the associated cleavage probability. Count data is min-max normalized
@@ -70,14 +95,7 @@ def plot_vplot(vplot_data: np.ndarray):
     y_vals = clave_prob[int(min_range)+int(midpoint):int(max_range)+int(midpoint)]
     x_vals = np.linspace(min_range, max_range, len(y_vals))
 
-    # Process v-plot count data for plotting
-    # imshow orients plots differently than the 2D histogram code used to generate array
-    # Here we reorient the data for ploting with imshow
-    vplot_data_rotated = np.flip(vplot_data.T)
-    # Min-max normalize counts
-    min_max_scaler = preprocessing.MinMaxScaler()
-    array_to_plot = min_max_scaler.fit_transform(vplot_data_rotated)
-
+    array_to_plot = process_vplot_data(vplot_data)  
 
     # Plot 
     fig, ax = plt.subplots(2,1,figsize = (9,6), gridspec_kw={'height_ratios': [1, 4]}, sharex=True)
@@ -127,4 +145,77 @@ def plot_fld(fld: np.ndarray = None):
     plt.show() 
     return
 
-def plot_composite(vplot_data: np.ndarray, fld: np.ndarray = None):
+def plot_composite(vplot_data: np.ndarray):#, fld: np.ndarray = None):
+    """
+    Generate a v_plot pdf that contains the associated cleavage probability. Count data is min-max normalized
+    
+    Parameters
+    ----------
+    vplot_data : np.ndarray
+    2D Numpy array that contains the data to be plotted.
+
+    Returns
+    -------
+    Generates .pdf
+
+    TO DO
+    -----
+    Improve handling of reading in fld
+    """
+
+    # Load cleavage probability array 
+    # Add functionilty to return error if file does not exist
+    clave_prob = np.load('cleavage_prob.npy')
+    min_range = -1. * distance_from_frag_center
+    max_range = distance_from_frag_center
+    midpoint = fiber_midpoint
+    # Subset cleavage probability array to values to plot
+    y_vals = clave_prob[int(min_range)+int(midpoint):int(max_range)+int(midpoint)]
+    x_vals = np.linspace(min_range, max_range, len(y_vals))
+
+    array_to_plot = process_vplot_data(vplot_data)  
+
+    # Plot 
+    # fig, ax = plt.subplots(2,1,figsize = (9,6), gridspec_kw={'height_ratios': [1, 4]}, sharex=True)
+    fig, (ax1, ax2) = plt.subplots(2,2,figsize = (11,6), gridspec_kw={'height_ratios': [1, 4], 'width_ratios': [9, 2]})
+    # First Subplot: Cleavage probability
+    ax1[0].plot(x_vals, y_vals)
+    ax1[0].set_ylabel('Cleavage \n Prob.')
+    ax1[0].set_xlim((min_range,max_range))
+    # Get rid of unwanted axes
+    ax1[0].spines['right'].set_visible(False)
+    ax1[0].spines['top'].set_visible(False)
+    ax2[1].spines['right'].set_visible(False)
+    ax2[1].spines['top'].set_visible(False)
+    #Clear top right plot
+    ax1[1].clear()
+    ax1[1].axis("off")
+    ax1[1].set_visible(False)
+    ax1[1].remove()
+
+    # Second Subplot: V-Plot
+    im = ax2[0].imshow(array_to_plot, cmap='seismic', extent = [min_range, max_range, 0, max_fragment_length], aspect = 1)
+    ax2[0].set_xlabel('Distance from Fiber Midpoint to Fragment Center (nt)')
+    ax2[0].set_ylabel('Fragment \n Length (nt)')
+    # Position and format color bar
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.22, 0.025, 0.33])
+    cbar = fig.colorbar(im, ax=ax2[0], cax=cbar_ax)#shrink=0.35)
+    cbar.set_label('Relative Counts')
+    # Set shared axes
+    ax1[0].sharex(ax2[0])
+    ax2[1].sharey(ax2[0])
+    # Got scaled value by guessing and checking.
+    ax2[1].set_box_aspect(1.875)
+
+    ax2[1].set_ylabel(' ')
+    # Plot fld
+    frag_lens = np.load('frag_lens.npy') # TO DO -- account for when fild does not exist
+    midpts = np.load('frag_midpts.npy') 
+    fm = frag_mid_df(frag_lens, midpts)
+    data_for_hist = pd.DataFrame(fm[fm.frag_len<max_fragment_length].frag_len)
+    sns.histplot(data=data_for_hist, y = 'frag_len', bins=100, stat= 'probability', ax=ax2[1])
+
+
+    plt.savefig('vplot_w_fld_and_cleavprob.pdf')
+    return
